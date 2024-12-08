c65304465867befb10f1137062775731a3372e19
fddcsjxk <lots.samuel@gmail.com>
2024-12-09 04:46:23 +0900

    updated tick

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 pygame.init()
+clock = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
