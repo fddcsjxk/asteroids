cf6a5f092e42740044069320f4ac264ace87bd1b
fddcsjxk <lots.samuel@gmail.com>
2024-12-11 11:31:09 +0900

    Turning

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 pygame.init()
 clock = pygame.time.Clock()
 dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 def main():
     print("Starting asteroids!")
@@ -17,8 +17,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
         dt = (clock.tick(60) / 1000)
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+      
+
 
 if __name__ == "__main__":
     main()
